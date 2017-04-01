9b5a327e64bc5de89c89ef97c569de332db27769
patrik <patrik@localhost>
2017-04-01 10:30:53 +0200

    Observed DistanceSensor

---
+++ Raspbian/Observer/ObservedSensors.py
@@ -0,0 +1,107 @@
+'''
+Created on 01.04.2017
+
+@author: patrik
+'''
+import time
+import sys
+sys.path += ['../util']
+from Observer import Observer, Observable
+sys.path += ['../sensors']
+from ObservedDistanceSensor import ObservedDistanceSensor
+
+class SensorObserver
+    def __init__(self):
+        self.isOpen = 0
+        self.openNotifier = Flower.OpenNotifier(self)
+        self.closeNotifier= Flower.CloseNotifier(self)
+    def open(self): # Opens its petals
+        self.isOpen = 1
+        self.openNotifier.notifyObservers()
+        self.closeNotifier.open()
+    def close(self): # Closes its petals
+        self.isOpen = 0
+        self.closeNotifier.notifyObservers()
+        self.openNotifier.close()
+    def closing(self): return self.closeNotifier
+    
+    class OpenNotifier(Observable):
+        def __init__(self, outer):
+            Observable.__init__(self)
+            self.outer = outer
+            self.alreadyOpen = 0
+        def notifyObservers(self):
+            if self.outer.isOpen and \
+            not self.alreadyOpen:
+                self.setChanged()
+                Observable.notifyObservers(self)
+                self.alreadyOpen = 1
+        def close(self):
+            self.alreadyOpen = 0
+
+    class CloseNotifier(Observable):
+        def __init__(self, outer):
+            Observable.__init__(self)
+            self.outer = outer
+            self.alreadyClosed = 0
+        def notifyObservers(self):
+            if not self.outer.isOpen and \
+            not self.alreadyClosed:
+                self.setChanged()
+                Observable.notifyObservers(self)
+                self.alreadyClosed = 1
+        def open(self):
+            self.alreadyClosed = 0
+
+        class OpenNotifier(Observable):
+        def __init__(self, outer):
+            Observable.__init__(self)
+            self.outer = outer
+            self.alreadyOpen = 0
+        def notifyObservers(self):
+            if self.outer.isOpen and \
+            not self.alreadyOpen:
+                self.setChanged()
+                Observable.notifyObservers(self)
+                self.alreadyOpen = 1
+        def close(self):
+            self.alreadyOpen = 0
+
+    class CloseNotifier(Observable):
+        def __init__(self, outer):
+            Observable.__init__(self)
+            self.outer = outer
+            self.alreadyClosed = 0
+        def notifyObservers(self):
+            if not self.outer.isOpen and \
+            not self.alreadyClosed:
+                self.setChanged()
+                Observable.notifyObservers(self)
+                self.alreadyClosed = 1
+        def open(self):
+            self.alreadyClosed = 0
+            
+            
+            
+if __name__ == '__main__':
+    try:
+        
+        so = SensorObserver()
+        lsens = ObservedDistanceSensor("LINKS",20,21)
+        rsens = ObservedDistanceSensor("RECHTS",19,26)
+        
+        so.openNotifier.addObserver(lsens.openObserver)
+        so.openNotifier.addObserver(rsens.openObserver)
+        so.closeNotifier.addObserver(lsens.closeObserver)
+        so.closeNotifier.addObserver(rsens.closeObserver)
+        
+        so.open()
+        
+        while True:
+            time.sleep(1)
+ 
+        # Beim Abbruch durch STRG+C resetten
+    except KeyboardInterrupt:
+        print("Sensorzentrale wird beendet")
+        so.close()
+       
---
+++ Raspbian/sensors/ObservedDistanceSensor.py
@@ -0,0 +1,81 @@
+#Bibliotheken einbinden
+import RPi.GPIO as GPIO
+import time
+import sys
+sys.path += ['../util']
+from Observer import Observer, Observable
+
+class ObservedDistanceSensor:
+	def __init__(self,name,trigger,echo):
+		self.SENSOR_NAME = name
+		#GPIO Pins zuweisen
+		self.GPIO_TRIGGER = trigger
+		self.GPIO_ECHO = echo
+		self.running = 0;
+		self.doSetup()
+		
+		self.openObserver = ObservedDistanceSensor.OpenObserver(self)
+        self.closeObserver = ObservedDistanceSensor.CloseObserver(self)
+
+	def doSetup(self):
+		#GPIO Modus (BOARD / BCM)
+		GPIO.setmode(GPIO.BCM)
+ 
+		#Richtung der GPIO-Pins festlegen (IN / OUT)
+		GPIO.setup(self.GPIO_TRIGGER, GPIO.OUT)
+		GPIO.setup(self.GPIO_ECHO, GPIO.IN)
+	
+	def getName(self):
+		return self.SENSOR_NAME 
+
+	def measure(self):
+    		# setze Trigger auf HIGH
+    		GPIO.output(self.GPIO_TRIGGER, True)
+ 
+    		# setze Trigger nach 0.01ms aus LOW
+    		time.sleep(0.00001)
+    		GPIO.output(self.GPIO_TRIGGER, False)
+ 
+    		StartZeit = time.time()
+    		StopZeit = time.time()
+ 
+    		# speichere Startzeit
+    		while GPIO.input(self.GPIO_ECHO) == 0:
+        		StartZeit = time.time()
+ 
+    		# speichere Ankunftszeit
+    		while GPIO.input(self.GPIO_ECHO) == 1:
+        		StopZeit = time.time()
+ 
+    		# Zeit Differenz zwischen Start und Ankunft
+    		TimeElapsed = StopZeit - StartZeit
+    		# mit der Schallgeschwindigkeit (34300 cm/s) multiplizieren
+    		# und durch 2 teilen, da hin und zurueck
+    		distance = (TimeElapsed * 34300) / 2
+ 
+    		return distance
+
+	def close(self):
+		self.running = 0;
+		GPIO.cleanup()
+		
+	# An inner class for observing openings:
+    class OpenObserver(Observer):
+        def __init__(self, outer):
+            self.outer = outer
+            self.outer.running = 1
+        def update(self, observable, arg):
+        	try:
+          		while self.outer.running==1:
+		            dist = self.outer.measure()
+		            print (self.outer.name + ": Gemessene Entfernung = %.1f cm" % dist)
+		            time.sleep(1)
+              
+    # Another inner class for closings:
+    class CloseObserver(Observer):
+        def __init__(self, outer):
+            self.outer = outer
+        def update(self, observable, arg):
+            print("Abstandsmessung '"+self.outer.name + "' wird abgeschaltet.")
+        	self.outer.close()
+
