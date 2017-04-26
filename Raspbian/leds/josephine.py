8a79d2d2e1b343c70c527f7d00f1407c6907e625
patrik <patrik@localhost>
2017-04-26 21:10:27 +0200

    ...

--- Raspbian/leds/josephine.py
+++ Raspbian/leds/josephine.py
@@ -311,7 +311,7 @@ if __name__ == "__main__":
     
     for i in range(10):
         #kitt_wheel(pixels,blink_times=2, wait=0.2, speed=0.04)
-        kitt_wheel(pixels,blink_times=2, wait=0.2, speed=0.04, color=DARKGREEN)
+        kitt_wheel(pixels,blink_times=2, wait=0.2, speed=0.04, color=SANDYBROWN)
         #kitt_wheel(pixels,blink_times=2, wait=0.7, speed=0.06, color=(247,141,3))
         #blink_police(pixels, blink_times = 2, wait = 0.2)
         # blink_color(pixels, blink_times = 1, color=(0, 255, 0))
